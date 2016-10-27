6cc09ad38ed143953ced3bee50c4df61e7d19232
536BLF <536blf@gmail.com>
2016-10-27 10:14:24 -0400

    Create BLF536.ino
    Outline for the class car and main file

---
+++ BLF536.ino
@@ -0,0 +1,27 @@
+class car
+{
+private:
+public:
+   void MotorDiff();
+   void sysId();
+};
+
+void car::MotorDiff()
+{
+//MotorDiff code here
+}
+
+void sysId()
+{
+//sysId code here
+}
+
+car BLF536;
+
+void setup(){
+//Setup code here
+}
+
+void loop(){
+//Loop code here
+}
