38eb86793182eb2271b0fb4a18a26cdac61778fa
536BLF <536blf@gmail.com>
2016-10-27 15:40:56 -0400

    Update BLF536.ino
    System identification code and main loop Rev 1. Still need sensor integration into code.

--- BLF536.ino
+++ BLF536.ino
@@ -1,27 +1,58 @@
+#define SETSPEED 175 //arbitrary
+#define ZEROREFTIME 1000 //Delay before unit step kicks in
+#define KPROP 100; //Proportional controller constant
+
 class car
 {
 private:
+   int pwmSetSpeed;
+   int motorDiffPWM;
+   double Kprop;
+   double refPoint;
+   double error;
+   double sensVal;
 public:
    void MotorDiff();
    void sysId();
+   void getVal();
+   car();
 };
+
+car::car()
+{
+  Kprop = KPROP;
+  sensVal = 0;
+  error = 0;
+  refPoint = 0;
+}
 
 void car::MotorDiff()
 {
 //MotorDiff code here
 }
 
-void sysId()
+void car::sysId()
 {
-//sysId code here
+  if(millis()<ZEROREFTIME) refPoint = 0; //Reference point is zero until time specified, then goes to 1. (step function)
+  else refPoint = 1;
+
+  motorDiffPWM = Kprop*error; //Proportional controller, error times a constant = motor differential
+}
+
+//Gets sensed position from the sensors and calculates error
+void car::getVal()
+{
+  error = refPoint - sensVal; //error is the reference point minus the sensed value
+  Serial.println(sensVal);
 }
 
 car BLF536;
 
 void setup(){
-//Setup code here
+  Serial.begin(9600);
 }
 
 void loop(){
-//Loop code here
+  BLF536.getVal();
+  BLF536.sysId();
 }
