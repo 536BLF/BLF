391004c0c7b445e0a6f111d290eae4874de39f55
536BLF <536blf@gmail.com>
2016-10-28 14:47:14 -0400

    Created Motordif() fn
    Takes the dif value and writes a speed to the PWM pins for either motor accordingly.
    
    -Ben, Catherine, Yung, Sam

--- BLF536.ino
+++ BLF536.ino
@@ -1,11 +1,16 @@
 #define SETSPEED 175 //arbitrary
 #define ZEROREFTIME 1000 //Delay before unit step kicks in
 #define KPROP 100; //Proportional controller constant
-
+#define DIRECTIONPIN1 7
+#define DIRECTIONPIN2 10
+// motor input1 and input2 they should be PWM signals
+#define SPEEDPIN1 3
+#define SPEEDPIN2 14
+// motor output1 and 2   those pins are decided by pin configuration in Datasheet
 class car
 {
 private:
-   int pwmSetSpeed;
+   int pwmSetSpeed=SETSPEED;
    int motorDiffPWM;
    double Kprop;
    double refPoint;
@@ -24,11 +29,23 @@ car::car()
   sensVal = 0;
   error = 0;
   refPoint = 0;
+  
+  
 }
 
 void car::MotorDiff()
 {
-//MotorDiff code here
+int RightSpeed;
+int LeftSpeed;
+
+//   assume diffvalue>0 as right turn and <0 is a right turn
+   LeftSpeed=this->pwmSetSpeed-(this->motorDiffPWM/2);
+   RightSpeed=this->pwmSetSpeed+(this->motorDiffPWM/2);
+   // 
+   digitalWrite(DIRECTIONPIN1,HIGH);
+   digitalWrite(DIRECTIONPIN2, LOW);
+   analogWrite(SPEEDPIN1,LeftSpeed);// write speed for motor1
+   analogWrite(SPEEDPIN1,LeftSpeed); // write speed for motor2 
 }
 
 void car::sysId()
@@ -50,6 +67,10 @@ car BLF536;
 
 void setup(){
   Serial.begin(9600);
+  pinMode(DIRECTIONPIN1, INPUT);
+  pinMode(DIRECTIONPIN2, INPUT);
+  pinMode(SPEEDPIN1, OUTPUT);
+  pinMode(SPEEDPIN2, OUTPUT);
 }
 
 void loop(){
