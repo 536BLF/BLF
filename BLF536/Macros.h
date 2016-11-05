5ab4d98dcbc34b0414a8a59646b824c17ad837a6
536BLF <536blf@gmail.com>
2016-11-05 13:38:59 -0400

    Create Macros.h
    Created the header file

---
+++ BLF536/Macros.h
@@ -0,0 +1,41 @@
+#define PIN_ANALOG_0 (A0)
+#define PIN_ANALOG_1 (A1)
+
+#define PIN_LED_0 (9)
+#define PIN_LED_1 (8)
+
+#define PIN_S2 (A5)
+#define PIN_S1 (A4)
+#define PIN_S0 (A3)
+
+#define PIN_LED_ARDUINO (13)
+
+#define MULTIPLIER (1000UL)
+
+#define MIN_SENSOR_VAL (0)
+#define MAX_SENSOR_VAL (1023)
+
+#define NUM_SAMPLES (4)
+#define SECONDS_8   (8000UL)
+
+#define ZERO_ERROR  (3500)
+#define ZERO_ERROR_1 (2407)
+#define ZERO_ERROR_0 (2475)
+
+#define BLACK_LINE (0)
+#define WHITE_LINE (1)
+
+#define ON_LINE_THRESHOLD (800)
+#define OFF_LINE_THRESHOLD (200)
+
+#define NUM_SENSORS (8)
+
+#define SETSPEED 175 //arbitrary
+#define ZEROREFTIME 1000 //Delay before unit step kicks in
+#define KPROP 100; //Proportional controller constant
+#define DIRECTIONPIN1 7
+#define DIRECTIONPIN2 10
+// motor input1 and input2 they should be PWM signals
+#define SPEEDPIN1 3
+#define SPEEDPIN2 14
+// motor output1 and 2   those pins are decided by pin configuration in Datasheet
